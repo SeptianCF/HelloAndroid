d825fab403266777d45a51057cfd9b333291d887
Septian Caesar Floresko <septian9caesar@gmail.com>
2019-09-05 14:34:51 +0700

    Pesan

--- app/src/main/java/com/example/tugasminggu1/MainActivity.java
+++ app/src/main/java/com/example/tugasminggu1/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.tugasminggu1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"App on Start",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "App On Stop", Toast.LENGTH_SHORT).show();
     }
 }
 
